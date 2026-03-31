10ffd51bfe848c0830f9daf441ff96f498ee85b7
Turma01-14 <julio3.oliveir@gmail.com>
2026-03-31 11:08:34 -0300

    feat: Adiciona a base do projeto

---
+++ SweetBites/SweetBites/SweetBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetBitesApp.swift
+//  SweetBites
+//
+//  Created by Turma01-14 on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SweetBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
