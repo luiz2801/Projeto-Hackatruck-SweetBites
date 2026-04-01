3fce1d915ce235a112b87345dea4399278d1ed36
Julio C. Oliveira <julio3.oliveir@gmail.com>
2026-04-01 15:00:30 -0300

    feat: Adiciona o ViewModel do recipes e do user.

--- SweetBites/SweetBites/VVMM/vm_recipes.swift
+++ SweetBites/SweetBites/VVMM/vm_recipes.swift
@@ -8,11 +8,62 @@
 import Foundation
 import Combine
 
-enum APIEndpointUser {
+class RecipesViewModel: ObservableObject {
+    @Published var recipes: [Recipes] = []
+    
+    private let service = RecipesService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    func fetch() {
+        guard let url = APIEndpointRecipes.get.url else { return }
+        
+        service.fetchRecipes(url: url)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }, receiveValue: { [weak self] recipes in
+                self?.recipes = recipes
+            })
+            .store(in: &cancellables)
+    }
+    
+    func post(recipe: Recipes) {
+        guard let url = APIEndpointRecipes.post.url else { return }
+        
+        service.addRecipe(recipe: recipe, url: url)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] _ in
+                self?.fetch()
+            }, receiveValue: { _ in })
+            .store(in: &cancellables)
+    }
+    
+    func put(recipe: Recipes) {
+        guard let url = APIEndpointRecipes.put.url else { return }
+        
+        service.updateRecipe(recipe: recipe, url: url)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] _ in
+                self?.fetch()
+            }, receiveValue: { _ in })
+            .store(in: &cancellables)
+    }
+    
+    func delete(recipe: Recipes) {
+        guard let url = APIEndpointRecipes.delete.url else { return }
+        
+        service.deleteRecipe(recipe: recipe, url: url)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] _ in
+                self?.fetch() 
+            }, receiveValue: { _ in })
+            .store(in: &cancellables)
+    }
+}
+
+enum APIEndpointRecipes {
     case get, post, put, delete
     
     private var baseURL: String { "http://127.0.0.1:1880" }
-    private var baseName: String { "SweetBitesUser" }
+    private var baseName: String { "SweetBitesRecipes" }
     
     var url: URL? {
         switch self {
