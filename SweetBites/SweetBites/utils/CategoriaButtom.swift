6ff225265e1f75fe4c34d1d49184d411c55dfc0a
Turma01-14 <julio3.oliveir@gmail.com>
2026-04-07 11:27:52 -0300

    feat: Adiciona o AddRecipesUI funcional.

--- SweetBites/SweetBites/utils/CategoriaButtom.swift
+++ SweetBites/SweetBites/utils/CategoriaButtom.swift
@@ -9,28 +9,28 @@ import Foundation
 
 import SwiftUI
 
-struct CategoriaButton: View {
-    var titulo: String
-    @Binding var isSelected: Bool
-    
-    var corSelecionado: Color = .red
-    var corBorda: Color = .pink
-    
-    var body: some View {
-        Button(action: {
-            isSelected.toggle()
-        }) {
-            Text(titulo)
-                .font(.subheadline)
-                .bold()
-                .padding(.horizontal, 20)
-                .padding(.vertical, 8)
-                .foregroundStyle(isSelected ? .white : corBorda)
-                .background(isSelected ? corSelecionado : Color.white)
-                .overlay(
-                    Capsule().stroke(corBorda, lineWidth: 1)
-                )
-                .clipShape(Capsule())
-        }
-    }
-}
+//struct CategoriaButton: View {
+//    var titulo: String
+//    @Binding var isSelected: Bool
+//    
+//    var corSelecionado: Color = .red
+//    var corBorda: Color = .pink
+//    
+//    var body: some View {
+//        Button(action: {
+//            isSelected.toggle()
+//        }) {
+//            Text(titulo)
+//                .font(.subheadline)
+//                .bold()
+//                .padding(.horizontal, 20)
+//                .padding(.vertical, 8)
+//                .foregroundStyle(isSelected ? .white : corBorda)
+//                .background(isSelected ? corSelecionado : Color.white)
+//                .overlay(
+//                    Capsule().stroke(corBorda, lineWidth: 1)
+//                )
+//                .clipShape(Capsule())
+//        }
+//    }
+//}
